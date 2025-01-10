12e0069cf54c4459e1c284be23f38567ffef43d1
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2025-01-10 13:44:02 -0600

    added css and JavaScript code for Login & SignUp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,56 @@
 import '../css/general.css';
-import '../css/login.css';
+import styles from '../css/login.module.css';
 import { useState } from 'react';
 import bcrypt from 'bcryptjs';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-export default function Login({ accounts = [] }) {
+export default function Login() {
     const navigate = useNavigate();
     const [regEmail, setRegEmail] = useState('');
     const [regPass, setRegPass] = useState('');
     const [loggedIn, setLoggedIn] = useState(false);
+    const [message, setMessage] = useState('');
+    /* Used for testing */
+    const [accounts, setAccounts] = useState([
+        {
+            FirstName: "Ta'Lee",
+            LastName: "Hollins",
+            Email: "taleeh98454@gmail.com",
+            Password: 'password1234'
+        },
+        {
+            FirstName: "John",
+            LastName: "Doe",
+            Email: "johnDoe123@gmail.com",
+            Password: "Somerandompass"
+        }
+    ])
 
-    async function handleSubmission(e) {
+    function goToSignUp(){
+        navigate('/SignUp');
+    }
+
+    async function onSubmit(e) {
         e.preventDefault();
-
-        for (let i = 0; i < accounts.length; i++) {
-            const isMatch = await bcrypt.compare(regPass, accounts[i].Password);
-
-            if (accounts[i].Email === regEmail && isMatch) {
+        
+        // Used for testing
+        for (let i = 0; i < accounts.length; i++){
+            if (accounts[i].Email === regEmail && accounts[i].Password === regPass){
+                console.log('Successfully logged in!');
                 setLoggedIn(true);
-                break;
+                navigate('/');
+                return;
+            } else{
+                
+                setMessage('No accounts found that match your Email and Password. Please try again.');
             }
         }
 
         setRegEmail('');
         setRegPass('');
+        setLoggedIn(false);
     }
 
     function goBack(){
@@ -32,32 +59,58 @@ export default function Login({ accounts = [] }) {
 
     return (
         <>
-            <header className="login-header">
-                <h3>Sign In</h3>
-                <p>Sign in to your existing account</p>
+            <header className={styles.loginHeader}>
+            <button className={styles.backBtn} onClick={goBack}>
+                <FontAwesomeIcon icon={faArrowLeft} /> Go back
+            </button>
+                <h3 className={styles.loginHeaderH3}>Sign In</h3>
+                <p className={styles.loginHeaderP}>Sign in to your existing account</p>
             </header>
-            <main className="container">
-                <div className="form-container">
-                    <form onSubmit={handleSubmission}>
-                        <label>Email Address</label>
-                        <input
-                            type="text"
-                            value={regEmail}
-                            onChange={(e) => setRegEmail(e.target.value)}
-                        />
-                        <label>Password</label>
-                        <input
-                            type="text"
-                            value={regPass}
-                            onChange={(e) => setRegPass(e.target.value)}
-                        />
-                        <button type="submit">Sign In</button>
-                    </form>
+            <main className={styles.container}>
+          <div className={styles.loginformContainer}>
+            <form className={styles.loginForm} onSubmit={onSubmit}>
+                {loggedIn ?
+                    <p></p>
+                :
+                    <div className={styles.errorMsg}>
+                        <p>
+                        {message}
+                        </p>
+                    </div>
+                }           
+              <label>Email Address</label>
+              <input
+                type="text"
+                value={regEmail}
+                onChange={(e) => setRegEmail(e.target.value)}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                value={regPass}
+                onChange={(e) => setRegPass(e.target.value)}
+              />
+            
+                {!regEmail.includes('@') || regPass === '' ?
+                <button type="submit" disabled>
+                    Login
+                </button>    
+                :
+                <button type="submit">
+                    Login
+                </button>
+                }
+                <div className={styles.navContainer}>
+                <p>
+                    New to our store?
+                </p>
+                <button onClick={goToSignUp} className={styles.signupNav}>
+                    Create an Account
+                </button>
                 </div>
-                <button className="back-btn" onClick={goBack}>
-                     Go Back
-                    </button>
-            </main>
-        </>
+            </form> 
+          </div>
+        </main>
+      </>
     );
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,10 @@
 import '../css/general.css';
-import '../css/signup.css';
+import styles from '../css/signup.module.css';
 import { useState } from 'react';
 import bcrypt from 'bcryptjs';
-import Login from './Login';
-import { Navigate, useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function SignUp() {
     const navigate = useNavigate();
@@ -11,8 +12,12 @@ export default function SignUp() {
     const [lName, setLName] = useState('');
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
-    const [accounts, setAccounts] = useState([]);
+    const [agree, setAgree] = useState(false);
   
+    function agreeCheckbox(){
+      setAgree((prev) => !prev);
+    }
+
     async function handlePassword(plainPassword){
         try{
             const saltRounds = 10;
@@ -23,18 +28,16 @@ export default function SignUp() {
         }
     }
 
+    function isValidEmail(email){
+        const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return validEmail.test(email);
+    }
+
     async function onSubmit(e) {
       e.preventDefault();
+
       const hashedPassword = await handlePassword(pass);
-        
-      const newAccount = {
-        FirstName: fName,
-        LastName: lName,
-        Email: email,
-        Password: hashedPassword,
-      };
   
-      setAccounts([...accounts, newAccount]);
       setFName('');
       setLName('');
       setEmail('');
@@ -47,49 +50,65 @@ export default function SignUp() {
   
     return (
       <>
-        <header className="signup-header">
-          <h3>Create Your Account</h3>
-          <p>You can start shopping with us once your account is finished!</p>
-        </header>
-        <main className="container">
-            
-          <div className="form-container">
-            <form onSubmit={onSubmit}>
-              <label>First Name</label>
-              <input
-                type="text"
-                value={fName}
-                onChange={(e) => setFName(e.target.value)}
-              />
-              <label>Last Name</label>
-              <input
-                type="text"
-                value={lName}
-                onChange={(e) => setLName(e.target.value)}
-              />
-              <label>Email Address</label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <label>Password</label>
-              <p>Must be at least 8 characters.</p>
-              <p>Must have at least one letter and one number</p>
-              <input
-                type="text"
-                value={pass}
-                onChange={(e) => setPass(e.target.value)}
-              />
-              <button type="submit">Create Account</button>
-            </form>
-            {/* <Login accounts={accounts} /> */}
+      <header className={styles.signupHeader}>
+          <button className={styles.backBtn} onClick={goBack}>
+              <FontAwesomeIcon icon={faArrowLeft} /> Go back
+          </button>
+          <h3 className={styles.headerH3}>Create Your Account</h3>
+          <p className={styles.headerP}>
+              You can start shopping with us once your account is finished!
+          </p>
+      </header>
+      <main className={styles.container}>
+          <div className={styles.formContainer}>
+              <form className={styles.signupForm} onSubmit={onSubmit}>
+                  <p className={styles.notice}>
+                      * Indicates required field
+                  </p>
+
+                  <label>First Name *</label>
+                  <input
+                      type="text"
+                      value={fName}
+                      onChange={(e) => setFName(e.target.value)}
+                  />
+                  <label>Last Name *</label>
+                  <input
+                      type="text"
+                      value={lName}
+                      onChange={(e) => setLName(e.target.value)}
+                  />
+                  <label>Email Address *</label>
+                  <input
+                      type="text"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <label>Password *</label>
+                  <p>Must be at least 8 characters.</p>
+                  <p>Must have at least one letter and one number</p>
+                  <input
+                      type="password"
+                      value={pass}
+                      onChange={(e) => setPass(e.target.value)}
+                  />
+
+                  <div className={styles.checkboxContainer}>
+                      <input type="checkbox" id={styles.formcheckbox} onChange={agreeCheckbox} />
+                      <p className={styles.checkboxP}>
+                          I agree to this store's Terms & Conditions and confirm that I want to join the rewards program.
+                      </p>
+                  </div>
+                  {!agree || fName === '' || lName === '' || !isValidEmail(email) || (pass.length < 8 || !/\d/.test(pass) || !/[a-zA-Z]/.test(pass)) ?
+                      <button type="submit" disabled>Create Account</button>
+                      :
+                      <button type="submit">
+                          Create Account
+                      </button>
+                  }
+              </form>
           </div>
-          {/* <Login accounts={accounts} /> */}
-        <button className="back-btn" onClick={goBack}>
-            Go Back
-        </button>
-        </main>
-      </>
+      </main>
+  </>
     );
   }
