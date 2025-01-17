fe7959a4f2eb95c782a6224bd30205e9e45db91c
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2025-01-17 16:45:29 -0600

    connected the sequelize code to the render database

--- server/db.mjs
+++ server/db.mjs
@@ -6,11 +6,12 @@ dotenv.config();
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
-      process.env.DB_NAME || '',
+      process.env.DB_DATABASE || '',
       process.env.DB_USER || '',
       process.env.DB_PASSWORD,
       {
-        host: 'localhost',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
         dialectOptions: {
           decimalNumbers: true,
